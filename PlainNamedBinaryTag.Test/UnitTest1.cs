e53895d590fffb0bb13c0ed2470ff47bc6291e46
tangge233 <tangge233@qq.com>
2025-06-04 23:21:20 +0800

    chore: add test proj

---
+++ PlainNamedBinaryTag.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using PlainNamedBinaryTag;
+
+namespace PlainNamedBinaryTag.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
