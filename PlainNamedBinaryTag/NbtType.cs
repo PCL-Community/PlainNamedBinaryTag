47e52f592d46634b5700837d89a81996046e9b0a
OpenKFC <168712411+Open-KFC@users.noreply.github.com>
2025-06-05 23:41:31 +0800

    实现简单的读写效果 (#1)

---
+++ PlainNamedBinaryTag/NbtType.cs
@@ -0,0 +1,19 @@
+﻿namespace PlainNamedBinaryTag
+{
+    public enum NbtType : byte
+    {
+        TEnd = 0,
+        TInt8 = 1,
+        TInt16 = 2,
+        TInt32 = 3,
+        TInt64 = 4,
+        TFloat32 = 5,
+        TFloat64 = 6,
+        TInt8Array = 7,
+        TString = 8,
+        TList = 9,
+        TCompound = 10,
+        TInt32Array = 11,
+        TInt64Array = 12,
+    }
+}
