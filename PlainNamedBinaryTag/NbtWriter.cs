2c44f72b7ed5cc61d639d2adf2a120c47c660a21
Open-KFC <1811593346@qq.com>
2025-06-07 14:13:23 +0800

    feat: 支持把 xml 写进 nbt

--- PlainNamedBinaryTag/NbtWriter.cs
+++ PlainNamedBinaryTag/NbtWriter.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
 using System.IO.Compression;
+using System.Linq;
+using System.Xml.Linq;
 using PlainNamedBinaryTag.Utils;
 
 namespace PlainNamedBinaryTag
@@ -47,6 +49,18 @@ namespace PlainNamedBinaryTag
                 _writer.Write(name);
             }
             WriteDynamicPayload(type, value);
+        }
+
+        public void WriteXmlNbt(XElement value)
+        {
+            if (value is null)
+                throw new ArgumentNullException(nameof(value));
+            var type = ParseToNbtType(value.Name.ToString());
+            WriteNbtType(type);
+            var name = value.Attribute("Name");
+            if (!(name is null))
+                _writer.Write(name.Value);
+            WriteDynamicNbtXml(value);
         }
 
         #region "write impl methods"
@@ -117,6 +131,89 @@ namespace PlainNamedBinaryTag
                 case NbtType.TInt64Array: WriteInt64ArrayPayload((NbtInt64Array)value); break;
                 default: throw new InvalidDataException();
             }
+        }
+
+        #endregion
+
+        #region xml write imple methods
+
+        private void WriteInt8ArrayNbtXml(XElement element)
+        {
+            var elementsArray = element.Elements().ToArray();
+            _writer.Write(elementsArray.Length);
+            foreach (var value in elementsArray)
+                _writer.Write(sbyte.Parse(GetFirstTextContent(value)));
+        }
+
+        private void WriteInt32ArrayNbtXml(XElement element)
+        {
+            var elementsArray = element.Elements().ToArray();
+            _writer.Write(elementsArray.Length);
+            foreach (var value in elementsArray)
+                _writer.Write(int.Parse(GetFirstTextContent(value)));
+        }
+
+        private void WriteInt64ArrayNbtXml(XElement element)
+        {
+            var elementsArray = element.Elements().ToArray();
+            _writer.Write(elementsArray.Length);
+            foreach (var value in elementsArray)
+                _writer.Write(long.Parse(GetFirstTextContent(value)));
+        }
+
+        private void WriteListNbtXml(XElement element)
+        {
+            WriteNbtType(ParseToNbtType(element.Attribute("ContentType").Value));
+            var elementsArray = element.Elements().ToArray();
+            _writer.Write(elementsArray.Length);
+            foreach (var value in elementsArray)
+                WriteDynamicNbtXml(value);
+        }
+
+        private void WriteCompoundNbtXml(XElement element)
+        {
+            var elementsArray = element.Elements().ToArray();
+            foreach (var child in elementsArray)
+            {
+                var type = ParseToNbtType(child.Name.ToString());
+                if (type == NbtType.TEnd)
+                    throw new InvalidDataException();
+                WriteNbtType(type);
+                _writer.Write(child.Attribute("Name").Value);
+                WriteDynamicNbtXml(child);
+            }
+            WriteNbtType(NbtType.TEnd);
+        }
+
+        private void WriteDynamicNbtXml(XElement element)
+        {
+            var type = (NbtType)Enum.Parse(typeof(NbtType), element.Name.ToString());
+            var firstTextValue = new Lazy<string>(() => GetFirstTextContent(element));
+            switch (type)
+            {
+                case NbtType.TInt8: _writer.Write(sbyte.Parse(firstTextValue.Value)); break;
+                case NbtType.TInt16: _writer.Write(short.Parse(firstTextValue.Value)); break;
+                case NbtType.TInt32: _writer.Write(int.Parse(firstTextValue.Value)); break;
+                case NbtType.TInt64: _writer.Write(long.Parse(firstTextValue.Value)); break;
+                case NbtType.TFloat32: _writer.Write(float.Parse(firstTextValue.Value)); break;
+                case NbtType.TFloat64: _writer.Write(double.Parse(firstTextValue.Value)); break;
+                case NbtType.TInt8Array: WriteInt8ArrayNbtXml(element); break;
+                case NbtType.TString: _writer.Write(firstTextValue.Value); break;
+                case NbtType.TList: WriteListNbtXml(element); break;
+                case NbtType.TCompound: WriteCompoundNbtXml(element); break;
+                case NbtType.TInt32Array: WriteInt32ArrayNbtXml(element); break;
+                case NbtType.TInt64Array: WriteInt64ArrayNbtXml(element); break;
+            }
+        }
+
+        private static string GetFirstTextContent(XElement element)
+        {
+            return element.Nodes().OfType<XText>().First().Value;
+        }
+
+        private static NbtType ParseToNbtType(string value)
+        {
+            return (NbtType)Enum.Parse(typeof(NbtType), value);
         }
 
         #endregion
